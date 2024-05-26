8c4b71c39bb2bff7137c60874cc52b419c591a28
0xBoredDev <caughmanconsulting@gmail.com>
2024-05-26 10:27:30 -0400

    initial commit
    setup nextjs app

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-3xl lg:flex">
+        <p className="flex w-full justify-center pb-6 pt-8">
+          Bored Map Coming Soon
+        </p>
+      </div>
+    </main>
+  );
+}
